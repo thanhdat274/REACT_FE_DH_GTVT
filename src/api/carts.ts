b829c297b2e30603da07e67e65f0a10cc65dadc3
duynvph13636 <93897038+duynvph13636@users.noreply.github.com>
2024-05-30 22:20:36 +0700

    Update-cart: call api update cart and delete cart

--- src/api/carts.ts
+++ src/api/carts.ts
@@ -9,3 +9,11 @@ export const getCartService = (idUser: string) => {
     const url = `/carts/users/${idUser}`
     return instance.get(url)
 }
+export const updateCartService = (data: any) => {
+    const url = `/carts/update`
+    return instance.put(url,data)
+}
+export const deleteCartService = (id: string) => {
+    const url = `/carts/delete/${id}`
+    return instance.delete(url)
+}
